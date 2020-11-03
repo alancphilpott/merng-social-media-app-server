4a7eef4aec5b4649d41d4f48b7c613f6d7fd6152
alancphilpott <alanp1995@gmail.com>
2020-11-03 23:43:16 +0000

    update port code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MONGODB } = require("./config");
 
 const pubSub = new PubSub();
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const server = new ApolloServer({
     typeDefs,
@@ -21,7 +21,7 @@ mongoose
     .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("⚽ Connected to MongoDB");
-        return server.listen({ port: PORT });
+        return server.listen({ port });
     })
     .then((res) => console.log(`⚡ Server Running @ ${res.url}`))
     .catch((err) => console.log(`Error Starting Server => ${err}`));
